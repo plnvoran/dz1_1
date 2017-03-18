cf825c75199f4a93d43cdd52818452992cdd0feb
plnvoran <plnvoran@mail.ru>
2017-03-18 22:58:49 +0300

    Вынес вспомогательные методы в TestBase

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/CntactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/CntactCreationTests.java
@@ -9,30 +9,9 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.*;
 
-public class CntactCreationTests {
-    FirefoxDriver wd;
-
-    @BeforeMethod
-    public void setUp() throws Exception {
-        wd = new FirefoxDriver();
-        wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-
-        login("admin", "secret");
+public class CntactCreationTests extends TestBase {
 
 
-    }
-
-    private void login(String username, String password) {
-        wd.get("http://localhost/addressbook/");
-        wd.findElement(By.name("user")).click();
-        wd.findElement(By.name("user")).clear();
-        wd.findElement(By.name("user")).sendKeys(username);
-        wd.findElement(By.id("LoginForm")).click();
-        wd.findElement(By.name("pass")).click();
-        wd.findElement(By.name("pass")).clear();
-        wd.findElement(By.name("pass")).sendKeys(password);
-        wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
-    }
 
     @Test
     public void testCntactCreation() {
@@ -43,47 +22,5 @@ public class CntactCreationTests {
         returnToHomePage();
     }
 
-    private void returnToHomePage() {
-        wd.findElement(By.linkText("home page")).click();
-    }
 
-    private void submitNewConract() {
-        wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
-    }
-
-    private void fillContactForm(CantactData cantactData) {
-        wd.findElement(By.name("firstname")).click();
-        wd.findElement(By.name("firstname")).clear();
-        wd.findElement(By.name("firstname")).sendKeys(cantactData.getFirstname());
-        wd.findElement(By.name("lastname")).click();
-        wd.findElement(By.name("lastname")).clear();
-        wd.findElement(By.name("lastname")).sendKeys(cantactData.getLastname());
-        wd.findElement(By.name("address")).click();
-        wd.findElement(By.name("address")).clear();
-        wd.findElement(By.name("address")).sendKeys(cantactData.getAddress());
-        wd.findElement(By.name("home")).click();
-        wd.findElement(By.name("home")).clear();
-        wd.findElement(By.name("home")).sendKeys(cantactData.getPhone());
-        wd.findElement(By.name("email")).click();
-        wd.findElement(By.name("email")).clear();
-        wd.findElement(By.name("email")).sendKeys(cantactData.getEmail());
-    }
-
-    private void initAddNew() {
-        wd.findElement(By.linkText("add new")).click();
-    }
-
-    @AfterMethod
-    public void tearDown() {
-        wd.quit();
-    }
-
-    public static boolean isAlertPresent(FirefoxDriver wd) {
-        try {
-            wd.switchTo().alert();
-            return true;
-        } catch (NoAlertPresentException e) {
-            return false;
-        }
-    }
 }
