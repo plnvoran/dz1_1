5dc39341543ae474bba2d034cad0d12029b6a1bd
plnvoran <plnvoran@mail.ru>
2017-03-24 20:10:26 +0300

    Добавлен BrowserType в конструктор ApplicationManager

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -17,12 +17,15 @@ public class ApplicationManager {
     private SessionHelper sessionHelper;
     private NavigationHelper navigationHelper;
     private GroupHelper groupHelper;
+    private String browser;
 
-
+    public ApplicationManager(String browser) {
+        this.browser = browser;
+    }
 
 
     public void init() {
-       String browser= BrowserType.FIREFOX;
+
        if (browser==BrowserType.FIREFOX) {
            wd = new FirefoxDriver();
        } else if (browser==BrowserType.CHROME){
