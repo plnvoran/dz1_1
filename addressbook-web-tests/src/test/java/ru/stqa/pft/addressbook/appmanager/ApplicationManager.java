46ce87b835e411dda192a270b3ac2446ddbb4c86
plnvoran <plnvoran@mail.ru>
2017-03-24 20:04:04 +0300

    Добавлен условный переход для выбора браузеров

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,6 +1,10 @@
 package ru.stqa.pft.addressbook.appmanager;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
@@ -8,7 +12,7 @@ import java.util.concurrent.TimeUnit;
  * Created by Denis on 19.03.2017.
  */
 public class ApplicationManager {
-    FirefoxDriver wd;
+    WebDriver wd;
     private ContactHelper contactHelper;
     private SessionHelper sessionHelper;
     private NavigationHelper navigationHelper;
@@ -18,7 +22,16 @@ public class ApplicationManager {
 
 
     public void init() {
-        wd = new FirefoxDriver();
+       String browser= BrowserType.FIREFOX;
+       if (browser==BrowserType.FIREFOX) {
+           wd = new FirefoxDriver();
+       } else if (browser==BrowserType.CHROME){
+           wd = new ChromeDriver();
+       } else if (browser==BrowserType.IE){
+           wd = new InternetExplorerDriver();
+       }
+
+
         wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
         groupHelper = new GroupHelper(wd);
         navigationHelper = new NavigationHelper(wd);
