fb7fa8eeb9ab6288f6da39a71147aeb44bf7790b
plnvoran <plnvoran@mail.ru>
2017-04-02 08:55:25 +0300

    Лямда выражение в создании группы

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -5,6 +5,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+
 import java.util.HashSet;
 import java.util.List;
 
@@ -30,7 +31,10 @@ public class GroupCreationTests extends TestBase {
             }
 
         }
-        group.setId(max);
+
+
+
+        group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
         before.add(group);
         Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
 
