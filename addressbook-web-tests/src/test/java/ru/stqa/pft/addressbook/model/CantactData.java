ff87164981643d3699775bbca104fb12eb993288
plnvoran <plnvoran@mail.ru>
2017-04-22 14:10:55 +0300

    Извлечение данных из ContactData

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/CantactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/CantactData.java
@@ -2,31 +2,82 @@ package ru.stqa.pft.addressbook.model;
 
 import com.thoughtworks.xstream.annotations.XStreamAlias;
 import com.thoughtworks.xstream.annotations.XStreamOmitField;
+import org.hibernate.annotations.Type;
 
+import javax.persistence.*;
 import java.io.File;
 @XStreamAlias("contact")
+@Entity
+@Table(name="addressbook")
+
 public class CantactData {
     @XStreamOmitField
+    @Id
+    @Column(name="id")
     private  int id=0;
+
+    @Column(name="firstname")
     private  String firstname;
+
+    @Column(name="lastname")
     private  String lastname;
+
+    @Transient
+    private  String group;
+
+    @Column(name="address")
+    @Type(type="text")
     private  String address;
+
+    @Column(name="home")
+    @Type(type="text")
     private  String homePhone;
+
+    @Column(name="mobile")
+    @Type(type="text")
     private  String mobilePhone;
+
+    @Column(name="work")
+    @Type(type="text")
     private  String workPhone;
 
+    @Override
+    public String toString() {
+        return "CantactData{" +
+                "id=" + id +
+                ", firstname='" + firstname + '\'' +
+                ", lastname='" + lastname + '\'' +
+                '}';
+    }
+
+    @Column(name="phone2")
+    @Type(type="text")
     private  String home2Phone;
 
+    @Transient
     private  String allPhones;
+
+    @Column(name="email")
+    @Type(type="text")
     private  String email;
+
+    @Column(name="email2")
+    @Type(type="text")
     private  String email2;
+
+    @Column(name="email3")
+    @Type(type="text")
     private  String email3;
 
+    @Transient
     private  String allEmail;
 
+    @Transient
     private  String withAllText;
 
-    private File photo;
+    @Column(name="photo")
+    @Type(type="text")
+    private String photo;
 
 
 
@@ -123,19 +174,9 @@ public class CantactData {
             }
 
     public CantactData withPhoto(File photo) {
-        this.photo = photo;
+        this.photo = photo.getPath();
         return this;
 
-    }
-
-
-
-    @Override
-    public String toString() {
-        return "CantactData{" +
-                "firstname='" + firstname + '\'' +
-                ", lastname='" + lastname + '\'' +
-                '}';
     }
 
 
@@ -194,7 +235,7 @@ public class CantactData {
         return withAllText;
     }
     public File getPhoto() {
-        return photo;
+        return new File(photo);
     }
 
 
